b6afbf6d93bacc9292195eb4d5735b386a33f208
Saksham Sharma <89659787+SakshamSharma07@users.noreply.github.com>
2025-10-21 15:56:28 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+
 const app = express();
 const PORT = 5000;
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
 
 app.get('/', (req, res) => {
   res.json({ message: "Hello from Backend 👋" });
 });
 
-app.listen(PORT, () => console.log(`Backend running on port ${PORT} and i love this project`));
+app.listen(PORT, () => 
+  console.log(`✅ Backend running on http://localhost:${PORT}`)
+);
